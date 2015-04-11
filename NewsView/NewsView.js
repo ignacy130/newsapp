050fa9fa479afdf4456633652343ea71fc0d0a7b
ignacy130 <ignacy130@gmail.com>
2015-04-11 22:18:40 +0200

    news adding and simple viewing

--- NewsView/NewsView.js
+++ NewsView/NewsView.js
@@ -1,21 +1,10 @@
 /**
  * Created by dabler on 11/04/15.
  */
-NewsDataBase = new Mongo.Collection("newsDataBase");
-
 if (Meteor.isClient) {
-    // This code only runs on the client
-    //Template.body.helpers({
-    //    newses: function () {
-    //        // Show newest tasks first
-    //        return NewsDataBase.find({}, {sort: {createdAt: -1}});
-    //    }
-    //});
-    Template.body.helpers({
-        newses: [
-            { header: "Wojna na bliskim wschodzie." },
-            { header: "Odkryto nowe złoża ropy na morzu północnym." },
-            { header: "Rosja atakuje polskę" }
-        ]
+    Template.NewsView.helpers({
+        allNewses: function () {
+            return Newses.find({});
+        }
     });
 }
