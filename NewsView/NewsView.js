26086c30aa4742c26449fc1acf4298ba4160f275
Dabler <podbielski.grzegorz@gmail.com>
2015-04-12 06:39:54 +0200

    Partially added ranking

--- NewsView/NewsView.js
+++ NewsView/NewsView.js
@@ -14,13 +14,15 @@ if (Meteor.isClient) {
     Template.NewsView.gestures({
         'swipeleft .myItem': function(e, t) {
             e.preventDefault();
-            var currentUserId = Meteor.user().userId
-            if(Newses.find({$or: [{votedFor: currentUserId},{votedAgainst: currentUserId}]}).count() == 0)
-            {
-
+            console.log(e.target);
+            if(Meteor.userId() != null) {
+                if(Newses.find({$or: [{votedFor: Meteor.userId()}, {votedAgainst: Meteor.userId()}]}).count()==0)
+                {
+                    console.log(e.target.id.valueOf());
+                    //Newses.update({ _id: id },{ $push: { votedAgainst: id }});
+                }
             }
             console.log("Swipe left!");
-
         },
         'swiperight .myItem': function(e, t) {
             e.preventDefault();
@@ -33,8 +35,6 @@ if (Meteor.isClient) {
         }
     });
 }
-
-
 
     if (Meteor.isCordova) {
         console.log("Printed only in mobile cordova apps");
