a4ce963168d94838b9715c139645bc9ef31c5d8c
Dabler <podbielski.grzegorz@gmail.com>
2015-04-12 10:25:40 +0200

    Not completed add rating method

--- NewsView/NewsView.js
+++ NewsView/NewsView.js
@@ -32,25 +32,64 @@ if (Meteor.isClient) {
             if(Meteor.userId() != null) {
                 if(Newses.find({$or: [{votedFor: Meteor.userId()}, {votedAgainst: Meteor.userId()}]}).count()==0)
                 {
-                    console.log(e.target.children[0].value);
-                    //Newses.update({ _id: id },{ $push: { votedAgainst: id }});
+                    var key = e.target.children[0].value
+                    Newses.update({ _id: key },{ $push: { votedAgainst: Meteor.userId() }});
+                    console.log("You voted against!");
                 }
+                else {console.log("You have voted already!");}
             }
-            console.log("Swipe left!");
         },
         'swiperight .myItem': function (e, t) {
             e.preventDefault();
-            var currentUserId = Meteor.user().userId
-            if (Newses.find({
-                    $or: [{
-                        votedFor: currentUserId
-                    }, {
-                        votedAgainst: currentUserId
-                    }]
-                }).count() == 0) {
-
+            if(Meteor.userId() != null) {
+                var key = e.target.children[0].value;
+                console.log(Meteor.userId());
+                console.log(Newses.find({$or: [{votedFor: Meteor.userId()}, {votedAgainst: Meteor.userId()}]}).count());
+                var keys = Newses.find({_id: key});
+                var vfor = keys.votedFor;
+                var vagainst = keys.votedAgainst;
+                var vforContains = false;
+                if(!(vfor === null || typeof vfor === 'undefined') && indexOf.call(vfor,key)>-1)
+                {
+                    vforContains = true;
+                }
+                var vagainstContains = false;
+                if(!(vagainst === null || typeof vagainst === 'undefined') && indexOf.call(vagainst, key)>-1)
+                {
+                    vagainstContains = true;
+                }
+                console.log(vforContains);
+                console.log(vagainstContains);
+                console.log(vforContains || vagainstContains);
+                if(!(vforContains || vagainstContains))
+                {
+                    Newses.update({ _id: key },{ $push: { votedFor: Meteor.userId() }});
+                    console.log("You voted for!");
+                }
+                else {console.log("You have voted already!");}
             }
-            console.log("Swipe right!");
         }
     });
+
+    var indexOf = function(needle) {
+        if(this === null) return -1;
+        if(typeof this === 'undefined') return -1;
+        if(typeof Array.prototype.indexOf === 'function') {
+            indexOf = Array.prototype.indexOf;
+        } else {
+            indexOf = function(needle) {
+                var i = -1, index = -1;
+                for(i = 0; i < this.length; i++) {
+                    if(this[i] === needle) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                return index;
+            };
+        }
+
+        return indexOf.call(this, needle);
+    };
 }
