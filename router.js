3f460fd034d6e5027a730a878831c114568e142d
ignacy130 <ignacy130@gmail.com>
2015-04-25 13:43:08 +0200

    register

--- router.js
+++ router.js
@@ -9,6 +9,7 @@ Router.map(function(){
     this.route('NewsView', {path:'/NewsView'});
     this.route('AddNewsView', {path:'/AddNewsView'});
     this.route('login', {path:'/login'});
+    this.route('register', {path:'/register'});
     this.route('NewsDetails', {path:'/NewsDetails/:someValue'});
     this.route('DeleteConfirmationView', {path:'/DeleteConfirmationView'});
 })
